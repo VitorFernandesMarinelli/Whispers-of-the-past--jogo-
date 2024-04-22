23e5458a52a26d21371df7ac3b8750c9caa5c85b
César Santana <18179457+cesarsantana2@users.noreply.github.com>
2024-04-22 10:37:12 -0300

    Update controle.py

--- scipts/controle.py
+++ scipts/controle.py
@@ -7,20 +7,20 @@ import grafico
 import time
 
 #Variaveis globais
-ehJogoExecultando = True
+ehJogoExecutando = True
 
 #Função para tratar os eventos capturados no modulo grafico
 def TratamentoEventos():
     #Entradas: Nenhuma
     #Saida: Nenhuma
-    global ehJogoExecultando
+    global ehJogoExecutando
     time.sleep(2) #Tem que dá um tempo para a janela ser criada
-    while ehJogoExecultando:
+    while ehJogoExecutando:
         if grafico.VerificadoNovoEvento(): #Se o modulo grafico captura um evento ele verifica qual é o tipo de evento
             evento = grafico.EntregaEventoAtual() 
             if evento.type == pygame.QUIT: #finaliza a aplicação ao apertar o botão de fechar
-                grafico.ParaExeculcao()
-                ehJogoExecultando = False
+                grafico.ParaExecucao()
+                ehJogoExecutando = False
 
 #Função para mandar um texto para ser escrito na tela para o modulo grafico, em um estilo de maquina de escrever (letra por letra)
 def Escrever(txt): #consertar depois
@@ -33,7 +33,7 @@ def Escrever(txt): #consertar depois
         time.sleep(0.5)
 
 #Crias trheads:
-threadGrafico = threading.Thread(target=grafico.ExecultarGrafico)
+threadGrafico = threading.Thread(target=grafico.ExecutarGrafico)
 threadEventos = threading.Thread(target=TratamentoEventos)
 threadEventos.start()
 threadGrafico.start()
